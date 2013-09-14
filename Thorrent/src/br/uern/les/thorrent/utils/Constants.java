18eb2ca0813b35dc94542327069d57453282a4bf
Irlan Arley <irlan.arley@gmail.com>
2013-09-14 16:13:16 -0300

    primeira versão da view

---
+++ Thorrent/src/br/uern/les/thorrent/utils/Constants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uern.les.thorrent.utils;
+
+/**
+ *
+ * @author Irlan
+ */
+public class Constants {
+    public static String VERSION = "v0.1";
+    public static String TITLE_APLICATION = "Thorrent";
+    
+}
